299722a3c8a38c2e1f7f7009e543af81d72664ac
Ahmad Faiz Kamaludin <ach.faiz.k@gmail.com>
2026-01-11 10:33:33 +0700

    feat: add unit test and update readme

---
+++ internal/dialect/dialect_test.go
@@ -0,0 +1,58 @@
+package dialect_test
+
+import (
+	"testing"
+
+	"github.com/akfaiz/migris/internal/dialect"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestDialectString(t *testing.T) {
+	tests := []struct {
+		dialect  dialect.Dialect
+		expected string
+	}{
+		{dialect.Postgres, "postgres"},
+		{dialect.MySQL, "mysql"},
+		{dialect.Unknown, ""},
+	}
+
+	for _, test := range tests {
+		result := test.dialect.String()
+		assert.Equal(t, test.expected, result)
+	}
+}
+
+func TestGooseDialect(t *testing.T) {
+	tests := []struct {
+		dialect  dialect.Dialect
+		expected string
+	}{
+		{dialect.Postgres, "postgres"},
+		{dialect.MySQL, "mysql"},
+		{dialect.Unknown, ""},
+	}
+
+	for _, test := range tests {
+		result := test.dialect.GooseDialect()
+		assert.Equal(t, test.expected, string(result))
+	}
+}
+
+func TestFromString(t *testing.T) {
+	tests := []struct {
+		input    string
+		expected dialect.Dialect
+	}{
+		{"postgres", dialect.Postgres},
+		{"pgx", dialect.Postgres},
+		{"mysql", dialect.MySQL},
+		{"mariadb", dialect.MySQL},
+		{"unknown", dialect.Unknown}, // default
+	}
+
+	for _, test := range tests {
+		result := dialect.FromString(test.input)
+		assert.Equal(t, test.expected, result)
+	}
+}
